2f8b8cfdf997c024a4140b5d475c497120050e79
Marc Salat <marcs14@gmail.com>
2024-09-24 15:54:12 +0200

    Refresh rate working

--- extension.js
+++ extension.js
@@ -20,6 +20,7 @@ import GObject from "gi://GObject";
 import St from "gi://St";
 import Clutter from "gi://Clutter";
 import GLib from "gi://GLib";
+import Gio from "gi://Gio";
 
 import {
   Extension,
@@ -40,11 +41,25 @@ const STATUS_FILE = "/status";
 
 const PowerTracker = GObject.registerClass(
   class PowerTracker extends PanelMenu.Button {
-    _init() {
-      // -------------
-      // CONFIGURATION This should be fixed in the future when settings are added.
-      // -------------
+    _init(settings) {
+      this._settings = settings;
 
+      // -------------
+      // CONFIGURATION
+      // -------------
+      // Settings
+      this._settings.bind(
+        "refreshrate",
+        this,
+        "refreshrate",
+        Gio.SettingsBindFlags.DEFAULT
+      );
+
+      this._settings.connect("changed::refreshrate", (settings, key) => {
+        this._set_timeout();
+      });
+
+      // Paths
       this.REAL_BAT_PATH = BAT0_PATH;
       this.POWER_NOW_EXISTS = false;
 
@@ -73,13 +88,30 @@ const PowerTracker = GObject.registerClass(
         y_align: Clutter.ActorAlign.CENTER,
       });
 
-      this._get_data();
       this.add_child(this._label);
 
-      this._timeout = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, 3, () => {
-        this._get_data();
-        return true;
-      });
+      // Preferences menu
+      this.menu.addAction(_("Preferences"), () => this.openPreferences());
+
+      this._get_data();
+      this._set_timeout();
+    }
+
+    _set_timeout() {
+      if (this._timeout) {
+        GLib.Source.remove(this._timeout);
+        this._timeout = null;
+      }
+
+      this.refreshrate = this._settings.get_int("refreshrate");
+      this._timeout = GLib.timeout_add_seconds(
+        GLib.PRIORITY_DEFAULT,
+        this.refreshrate,
+        () => {
+          this._get_data();
+          return true;
+        }
+      );
     }
 
     _get_data() {
@@ -145,55 +177,12 @@ const PowerTracker = GObject.registerClass(
 
 export default class PowerTrackerExtension extends Extension {
   enable() {
-    this._powertracker = new PowerTracker();
+    this._powertracker = new PowerTracker(this.getSettings());
     Main.panel.addToStatusArea(this.uuid, this._powertracker);
-
-    // // Add a menu item to open the preferences window
-    // this._powertracker.menu.addAction(_("Preferences"), () =>
-    //   this.openPreferences()
-    // );
-
-    // // Create a new GSettings object, and bind the "show-indicator"
-    // // setting to the "visible" property.
-    // this._settings = this.getSettings();
-    // this._settings.bind(
-    //   "refreshrate",
-    //   this._powertracker,
-    //   "refreshrate",
-    //   Gio.SettingsBindFlags.DEFAULT
-    // );
-
-    // // Watch for changes to a specific setting
-    // this._settings.connect("changed::refreshrate", (settings, key) => {
-    //   console.debug("---pepe");
-    //   console.debug(`${key} = ${settings.get_value(key).print(true)}`);
-    // });
-
-    //---------- TUTORIAL EXAMPLE----------
-    this._powertracker.menu.addAction(_("Preferences"), () =>
-      this.openPreferences()
-    );
-
-    // Create a new GSettings object, and bind the "show-indicator"
-    // setting to the "visible" property.
-    this._settings = this.getSettings();
-    this._settings.bind(
-      "show-indicator",
-      this._powertracker,
-      "visible",
-      Gio.SettingsBindFlags.DEFAULT
-    );
-
-    // Watch for changes to a specific setting
-    this._settings.connect("changed::show-indicator", (settings, key) => {
-      console.debug(`${key} = ${settings.get_value(key).print(true)}`);
-    });
-    //---------- TUTORIAL EXAMPLE----------
   }
 
   disable() {
     this._powertracker.destroy();
     this._powertracker = null;
-    this._settings = null;
   }
 }
--- prefs.js
+++ prefs.js
@@ -8,45 +8,8 @@ import {
 } from "resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js";
 
 export default class PowerTrackerPreferences extends ExtensionPreferences {
-  // fillPreferencesWindow(window) {
-  //   window._settings = this.getSettings();
-  //   const page = new Adw.PreferencesPage({
-  //     title: _("General"),
-  //     icon_name: "dialog-information-symbolic",
-  //   });
-  //   window.add(page);
-
-  //   const group = new Adw.PreferencesGroup({
-  //     title: _("Settings"),
-  //     description: _("Configure the extension"),
-  //   });
-  //   page.add(group);
-
-  //   const refreshRate = new Adw.SpinRow({
-  //     title: _("Refresh Rate"),
-  //     adjustment: new Gtk.Adjustment({
-  //       lower: 1,
-  //       upper: 60,
-  //       step_increment: 1,
-  //       page_increment: 1,
-  //       page_size: 0,
-  //     }),
-  //     value: window._settings.get_int("refreshrate"),
-  //   });
-
-  //   group.add(refreshRate);
-
-  //   window._settings.bind(
-  //     "refreshrate",
-  //     refreshRate,
-  //     "value",
-  //     Gio.SettingsBindFlags.DEFAULT
-  //   );
-  // }
-
-  //---------- TUTORIAL EXAMPLE----------
   fillPreferencesWindow(window) {
-    // Create a preferences page, with a single group
+    window._settings = this.getSettings();
     const page = new Adw.PreferencesPage({
       title: _("General"),
       icon_name: "dialog-information-symbolic",
@@ -54,26 +17,30 @@ export default class PowerTrackerPreferences extends ExtensionPreferences {
     window.add(page);
 
     const group = new Adw.PreferencesGroup({
-      title: _("Appearance"),
-      description: _("Configure the appearance of the extension"),
+      title: _("Settings"),
+      description: _("Configure the extension"),
     });
     page.add(group);
 
-    // Create a new preferences row
-    const row = new Adw.SwitchRow({
-      title: _("Show Indicator"),
-      subtitle: _("Whether to show the panel indicator"),
+    const refreshRate = new Adw.SpinRow({
+      title: _("Refresh Rate"),
+      adjustment: new Gtk.Adjustment({
+        lower: 1,
+        upper: 60,
+        step_increment: 1,
+        page_increment: 1,
+        page_size: 0,
+      }),
+      value: window._settings.get_int("refreshrate"),
     });
-    group.add(row);
 
-    // Create a settings object and bind the row to the `show-indicator` key
-    window._settings = this.getSettings();
+    group.add(refreshRate);
+
     window._settings.bind(
-      "show-indicator",
-      row,
-      "active",
+      "refreshrate",
+      refreshRate,
+      "value",
       Gio.SettingsBindFlags.DEFAULT
     );
   }
-  //---------- TUTORIAL EXAMPLE----------
 }
